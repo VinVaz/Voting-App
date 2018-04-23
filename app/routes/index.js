f52250c2327ab83986005b8fa3a0ff044053c5f2
VinVaz <viniciusvazpro@gmail.com>
2018-04-23 17:37:54 -0300

    adds a poll button that redirects to an user's poll chart

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,7 +6,7 @@ module.exports = function(app){
 	app.route('/').get(function(req, res){
 		res.sendFile(path + '/public/index.html');
 	});
-	app.route('/HOME').get(function(req, res){
+	app.route('/home').get(function(req, res){
 	    res.redirect('/');
 	});
 	app.route('/profile').get(function(req, res){
