8465dd492e6c1b2185cd4d2e63606aed95ef9454
VinVaz <viniciusvazpro@gmail.com>
2018-04-27 20:23:55 -0300

    home page entries created

--- app/controllers/pollController_client.js
+++ app/controllers/pollController_client.js
@@ -4,26 +4,25 @@
 	var boxSpace = document.getElementById("poll-boxes-space");
 	var apiUrl = appUrl + '/api/polls';
 	
-	function createNewPollBox(){
-		var link = document.createElement('A');
+	function createNewPollBox(name){
+		var newLink = document.createElement('A');
 		//change this attribute later so that it can serve a specific user:
-		link.setAttribute("href", "/profile");
+		newLink.setAttribute("href", "/profile");
 		
-		var button = document.createElement('BUTTON');
-		button.setAttribute("class", "poll-box-button");
-        
-		link.appendChild(button);
-		boxSpace.appendChild(link);
+		var newButton = document.createElement('BUTTON');
+		newButton.setAttribute("class", "poll-box-button");
+		newButton.innerHTML = name;
+		
+		newLink.appendChild(newButton);
+		boxSpace.appendChild(newLink);
 	}
+	
 	function updatePollBox(data){
-			console.log("Im running");
 		var pollObject = JSON.parse(data);
-		var poll = pollObject.poll.name;
-		createNewPollBox();
-		//change this query selector so that it can perform 
-		//changes for all of the buttons that will be created:
-		var pollButton = document.querySelector(".poll-box-button");
-		pollButton.innerHTML = poll;
+		for(var i = 0; i < pollObject.length; i++){
+			var pollName = pollObject[i].name;
+		    createNewPollBox(pollName);
+		}	
 	}
 
 	ajaxFunctions.ready(ajaxFunctions.newRequest('GET', apiUrl, updatePollBox));
