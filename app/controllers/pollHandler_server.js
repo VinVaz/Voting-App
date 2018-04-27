9b1896a6c27fe24525595fdcacecd1a19b0fae8a
VinVaz <viniciusvazpro@gmail.com>
2018-04-27 15:56:36 -0300

    update

--- app/controllers/pollHandler_server.js
+++ app/controllers/pollHandler_server.js
@@ -7,7 +7,7 @@ function PollHandler(){
     
 	this.addOption = function(req, res){  
 		Users
-		    .findOneAndUpdate({"poll.name": "Biggest Country"}, {$push: {"poll.options": {"name":"canada", "clicks": 0}}}, {'_id': false})
+		    .findOneAndUpdate({"poll.options.name": {$ne:"canada"}}, {$push: {"poll.options": {"name":"canada", "clicks": 0}}})
 			.exec(function(err, result){
 			    if(err){throw err;}
 			    res.json(result);
@@ -24,7 +24,7 @@ function PollHandler(){
 	}; 
 	this.addPoll = function(req, res){
 		Users
-		    .create({"poll": {"name": "Best food", "options": []}})
+		    .create({"poll": {"name": "Biggest Country", "options": []}})
 			.exec(function(err, result){
 			    if(err){throw err;}
 			    res.json(result);
@@ -32,7 +32,7 @@ function PollHandler(){
 	};
 	this.deletePoll = function(req, res){	
 		Users    
-			.deleteOne({"poll.name":"Best food"})
+			.deleteOne({"poll.name":"Biggest Country"})
 			.exec(function(err, result){
 			    if(err){throw err;}
 			    res.json(result);
