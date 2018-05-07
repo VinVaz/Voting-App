24094c7ada781268c1a696f87cd67abab90f49c4
VinVaz <viniciusvazpro@gmail.com>
2018-05-07 01:30:12 -0300

    updates polls api

--- app/controllers/pollHandler_server.js
+++ app/controllers/pollHandler_server.js
@@ -4,16 +4,23 @@ var path = process.cwd();
 var Users = require('../models/users.js');
 
 module.exports = function pollApiServer(req, res){  
-		Users
-	   .find({}, {'_id': false, 'poll.name': true})
-			.exec(function(err, result){
-			    if(err){throw err;}
-				var message = [];
-				for(var i = 0; i < result.length; i++){
-					message[i] = result[i].poll;
-				}
-			    res.json(message);
-		    });
+  Users
+    .find({}, {'_id': false, 'polls.name': true})
+      .exec(function(err, result){
+        if(err){throw err;}
+        var message = [];
+		for(var i = 0; i < result.length; i++){
+          var userArr = result[i].polls;
+		  if(userArr){
+			  for(var j = 0; j < userArr.length; j++){
+			    message.push(userArr[j]);
+		      } 
+		  }
+		 
+        }
+        res.json(message);
+      });
 };
+
 
 
